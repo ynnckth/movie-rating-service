57d2c1bc592369a0122a99d91bfd52e2a24c75a6
Matthias Brun <matthias.brun@gmail.com>
2017-08-30 10:28:11 +0200

    Update OmdbApi

--- src/main/java/com/zuehlke/rating/omdb/OmdbApiClient.java
+++ src/main/java/com/zuehlke/rating/omdb/OmdbApiClient.java
@@ -4,6 +4,6 @@ import feign.Param;
 import feign.RequestLine;
 
 public interface OmdbApiClient {
-    @RequestLine("GET /?i={imdbId}")
+    @RequestLine("GET /?i={imdbId}&apikey=53f5b894")
     OmdbMovie getMovieByImdbId(@Param("imdbId") String imdbId);
 }
